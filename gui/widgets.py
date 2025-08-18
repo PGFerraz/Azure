0e3fa7c09f4662a408dce560679afa5bd468db4d
Pedro Gabriel Ferraz Santos Silva <144126732+PGFerraz@users.noreply.github.com>
2025-08-18 17:32:34 -0300

    Create widgets.py

---
+++ gui/widgets.py
@@ -0,0 +1,8 @@
+# Arquivo de configuração dos Widgets principais
+from kivy.uix.boxlayout import BoxLayout
+
+# Classe que contém os widgets principais
+class MainLayout(BoxLayout):
+    # inicializando a classe
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
